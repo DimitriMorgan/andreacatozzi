283348d0e54ef77c224b09e284dc1374002c30ba
gauvin <gauvin.thibaut83@gmail.com>
2015-04-18 22:51:08 +0200

    contact page now send message

--- src/AndreaCatozzi/FrontEndBundle/Controller/DefaultController.php
+++ src/AndreaCatozzi/FrontEndBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace AndreaCatozzi\FrontEndBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AndreaCatozzi\FrontEndBundle\Entity\contact;
+use AndreaCatozzi\FrontEndBundle\Form\contactType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -16,9 +19,34 @@ class DefaultController extends Controller
         return $this->render('AndreaCatozziFrontEndBundle:Default:biography.html.twig');
     }
 
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('AndreaCatozziFrontEndBundle:Default:contact.html.twig');
+        $contact = new contact();
+        $form = $this->createForm(new contactType(), $contact);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Hey bro, tu a un nouveau message d\'un visiteur sur ton site !')
+                ->setFrom('andrea-catozzi@site.com')
+                ->setTo('arkezis@hotmail.fr')
+                ->setBody($this->renderView('AndreaCatozziFrontEndBundle:Default:email.txt.twig', array('contact' => $contact)));
+
+            $message->setContentType("text/html");
+            $this->get('mailer')->send($message);
+
+//            $request->getSession()
+//                ->getFlashBag()
+//                ->add('success', 'Your Message has been successfully send')
+//            ;
+
+            return $this->redirect($this->generateUrl('front_end_contact'));
+        }
+
+        return $this->render('AndreaCatozziFrontEndBundle:Default:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+
     }
 
     public function picturesAction()
